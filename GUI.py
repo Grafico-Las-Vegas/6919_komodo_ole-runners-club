cdf82ab2309e23864b5d99ec45d9127d90070973
Blaze Sanders <blaze.d.a.sanders@gmail.com>
2025-12-05 15:29:27 -0800

    Start GUI and GPIO defination

--- GUI.py
+++ GUI.py
@@ -1,5 +1,33 @@
+#!/usr/bin/env python3
 
+# Standard Library
+from time import sleep
+
+# 3rd Party Libraries
 from nicegui import ui
 
+# Internal Libraries
+
 class GUI:
-  pass
+
+    def __init__(self):
+        self.fanRunDurationInSeconds = 7
+        self.ledBrightnessPercentage = 1.0
+
+
+    def layout(self):
+
+        with ui.row():
+            ui.label('Fan Duration:')
+            ui.select([1, 5, 10], value=1)
+        with ui.row():
+            ui.label('LED Brightness:')
+            ui.slider(min=0, max=100, value=100)
+
+    def start(self):
+        ui.run(dark=True, title='Komodo Ole Runners Club Project', window_size=(1920, 1080))
+
+if __name__ in {"__main__", "__mp_main__"}:
+    gui = GUI()
+    gui.layout()
+    gui.start()
--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 #  Komodo Ole Runners Club Project
+
+## Hardware
+- Raspberry Pi 5 
+- GPIO Header
+- 8 Channel Relay Module
+- Button with LED
+
 ## Front End
+Nice GUI - https://nicegui.io/
 
 ## Back End
+sudo apt update
+sudo apt upgrade
+sudo apt install python3-gpiozero
