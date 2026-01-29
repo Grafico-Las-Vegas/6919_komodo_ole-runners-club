1a3afe77a4cb0cc96529ab15249fa280d74cf523
Blaze <Blaze@MacBookAir.local>
2026-01-28 19:42:20 -0800

    Defined new LEDs, backend control loop, and defined class linking

--- GUI.py
+++ GUI.py
@@ -6,71 +6,120 @@ from time import sleep
 # 3rd Party Libraries
 from nicegui import ui
 
-# Internal Libraries
+MAX_DURATION = 6
+DEFAULT_DURATION = 3
+MIN_DURATION = 1
+
+MAX_BRIGHTNESS = 100
+MIN_BRIGHTNESS = 0
+
 
 class GUI:
-
-    buttons = ['On', 'Off']
+    powerButtonState = ["On", "Off"]
 
     def __init__(self):
         self.isFanOn = False
-        self.fanRunDurationInSeconds = 7
-        self.ledBrightnessPercentage = 100
+        self.fanRunDurationInSeconds = 3
+        self.fanSpeedInPercentage = 100
+        self.whiteLedBrightnessInPercentage = 100
+        self.redLedBrightnessInPercentage = 100
 
         # Button element references
         self.on_button = None
         self.off_button = None
 
-    def update_fan_config(self, duration):
+    def update_fan_config(self, duration, speed):
         self.fanRunDurationInSeconds = duration
-        print(f"Fan Duration updated to {duration} seconds")
+        self.fanSpeedInPercentage = speed
+        print(f"Fan Duration updated to {duration} seconds, at speed of {speed}")
 
-    def update_led_config(self, brightness):
-        self.ledBrightnessPercentage = brightness
-        print(f"LED Brightness updated to {brightness}%")
+    def update_led_config(self, color="white", brightness=100):
+        if color == "white":
+            self.whiteLedBrightnessInPercentage = brightness
+            print(f"White Overhead LED Brightness updated to {brightness}%")
+        elif color == "red":
+            self.redLedBrightnessInPercentage = brightness
+            print(f"Red Button LED Brightness updated to {brightness}%")
+        else:
+            print("Warning: LED Brightness not updated!")
 
-        def update_button_colors(self):
-            """Set button classes according to current `isFanOn` state."""
-            if self.on_button is None or self.off_button is None:
-                return
+    def update_gui_button_colors(self):
+        """Set button classes according to current `isFanOn` state."""
+        if self.on_button is None or self.off_button is None:
+            return
 
-            if self.isFanOn:
-                # On is active -> make it green, restore Off to its base red
-                self.on_button.classes('!bg-green-600 w-1/4')
-                self.off_button.classes('!bg-red-900 w-1/4')
-            else:
-                # Off is active -> make it green, restore On to its base blue
-                self.off_button.classes('!bg-green-600 w-1/4')
-                self.on_button.classes('!bg-blue-900 w-1/4')
+        if self.isFanOn:
+            # On is active -> make it green, restore Off to its base red
+            self.on_button.classes("!bg-green-600 w-1/4")
+            self.off_button.classes("!bg-red-900 w-1/4")
+        else:
+            # Off is active -> make it green, restore On to its base blue
+            self.off_button.classes("!bg-green-600 w-1/4")
+            self.on_button.classes("!bg-blue-900 w-1/4")
 
+    def button_pressed(self, key):
+        print(f"{key} Button clicked")
+        if key == "On":
+            self.isFanOn = True
+        elif key == "Off":
+            self.isFanOn = False
 
-        def button_pressed(self, key):
-            print(f"{key} Button clicked")
-            if key == 'On':
-                self.isFanOn = True
-            elif key == 'Off':
-                self.isFanOn = False
+        self.update_gui_button_colors()
 
-            self.update_button_colors()
+    def layout(self, gui):
+        with ui.column().classes("w-full gap-8"):
+            with ui.row().classes("w-full items-center"):
+                ui.label("Fan Duration (seconds):").classes("w-1/4 text-lg")
+                ui.select(
+                    [1, 3, 6],
+                    value=3,
+                    on_change=lambda e: gui.update_fan_config(
+                        e.value, self.fanSpeedInPercentage
+                    ),
+                )
 
-        def layout(self, gui):
+            with ui.row().classes("w-full items-center"):
+                ui.label("Fan Speed (%):").classes("w-1/4 text-lg")
+                ui.select(
+                    [0, 33, 66, 100],
+                    value=3,
+                    on_change=lambda e: gui.update_fan_config(
+                        self.fanRunDurationInSeconds, e.value
+                    ),
+                )
 
-            with ui.column().classes('w-full gap-8'):
-                with ui.row().classes('w-full items-center'):
-                    ui.label('Fan Duration (seconds):').classes('w-1/4 text-lg')
-                    ui.select([1, 5, 10], value=1, on_change=lambda e: gui.update_fan_config(e.value))
+            with ui.row().classes("w-full items-center"):
+                ui.label("White LED Brightness:").classes("w-1/4 text-lg")
+                ui.slider(min=0, max=100, value=100).classes("w-1/4").on(
+                    "update:model-value",
+                    lambda e: gui.update_led_config(e.args),
+                    throttle=1.0,
+                )
 
-                with ui.row().classes('w-full items-center'):
-                    ui.label('White LED Brightness:').classes('w-1/4 text-lg')
-                    ui.slider(min=0, max=100, value=100).classes('w-1/4').on('update:model-value', lambda e: gui.update_led_config(e.args), throttle=1.0)
+            with ui.row().classes("w-full items-center"):
+                ui.label("Red Button LED Brightness:").classes("w-1/4 text-lg")
+                ui.slider(min=0, max=100, value=100).classes("w-1/4").on(
+                    "update:model-value",
+                    lambda e: gui.update_led_config(e.args),
+                    throttle=1.0,
+                )
 
-                with ui.row().classes('w-full items-center'):
-                     self.on_button = ui.button('Turn On Fans', on_click=lambda: gui.button_pressed('On')).classes('!bg-blue-900 w-1/4')
-                     self.off_button = ui.button('Turn Off Fans', on_click=lambda: gui.button_pressed('Off')).classes('!bg-red-900 w-1/4')
+            with ui.row().classes("w-full items-center"):
+                self.on_button = ui.button(
+                    "Turn On Fans", on_click=lambda: gui.button_pressed("On")
+                ).classes("!bg-blue-900 w-1/4")
+                self.off_button = ui.button(
+                    "Turn Off Fans", on_click=lambda: gui.button_pressed("Off")
+                ).classes("!bg-red-900 w-1/4")
 
-
-    def start(self):
-        ui.run(native=False, dark=True, title='Komodo Ole Runners Club Project', window_size=(940, 640))
+        def start(self):
+            # TODO: Make full screen on display selected
+            ui.run(
+                native=False,
+                dark=True,
+                title="Komodo Ole Runners Club Project",
+                window_size=(940, 640),
+            )
 
 
 if __name__ in {"__main__", "__mp_main__"}:
--- OleRunnersClub.py
+++ OleRunnersClub.py
@@ -1,42 +1,100 @@
+#!/usr/bin/env python3
 
-# https://gpiozero.readthedocs.io/en/stable/
-from gpiozero import Button
+# Standard Library
 from time import sleep
 
+# 3rd Party Libraries
+from gpiozero import PWMLED, Button, Device, Fan, MockFactory
 
-MAX_DURATION = 10
-MIN_DURATION = 1
+# Internal Libraries
+import GUI
 
+# Global Objects
 # Board numbering https://gpiozero.readthedocs.io/en/stable/recipes.html#pin-numbering
-leftLED = PWMLED("BOARD13")
-rightLED = PWMLED("BOARD15")
-leftActionButton = Button("BOARD7")
-rightActionButton = Button("BOARD11")
-leftFan = Fan("BOARD16")
-rightFan = Fan("BOARD18")
+overheadWhiteLED = PWMLED("BOARD13")
+leftButtonLED = PWMLED("BOARD13")
+rightButtonLED = PWMLED("BOARD15")
+leftButtonAction = Button("BOARD7")
+rightButtonAction = Button("BOARD11")
+fan0 = Fan("BOARD16")  # or PWMLED() maybe
+fan1 = Fan("BOARD18")  # or PWMLED() maybe
+
+# Global Variables
+isDoorReady = False  # Door is open, not ready for fan blowing
+currentFanState = False
+currentFanDuration = GUI.DEFAULT_DURATION
+currentFanPercentage = GUI.MAX_DURATION
+currentButtonLedBrightness = GUI.MAX_BRIGHTNESS
+currentOverheadBrightness = GUI.MAX_BRIGHTNESS
+
+# Button Array Indicies
+BRIGHTNESS = 0
+ACTION = 1
 
 
-duration = 7
+def turn_on_fans(fans):
+    if currentFanDuration > GUI.MAX_DURATION:
+        currentFanDuration = GUI.MAX_DURATION
+    if currentFanDuration <= 0:
+        currentFanDuration = GUI.MIN_DURATION
 
-def turn_on_fan(duration=7):
-    if duration > MAX_DURATION:
-        duration = MAX_DURATION
-    if duration < 0:
-        duration = MIN_DURATION
+    for fan in fans:
+        fan.on()
 
-    leftFan.on()
-    rightFan.on()
-    sleep(duration)
-    leftFan.off()
-    rightFan.off()
+    sleep(currentFanDuration)
+
+    for fan in fans:
+        fan.off()
+
+
+def update_button_brightness(buttons, percentage=GUI.MAX_BRIGHTNESS):
+    if percentage > GUI.MAX_BRIGHTNESS:
+        percentage = GUI.MAX_BRIGHTNESS
+    if percentage < 0:
+        percentage = GUI.MIN_BRIGHTNESS
+
+    for button in buttons:
+        button[BRIGHTNESS].pwm(percentage)
+
+
+def update_overhead_brightness(percentage=GUI.MAX_BRIGHTNESS):
+    if percentage > GUI.MAX_BRIGHTNESS:
+        percentage = GUI.MAX_BRIGHTNESS
+    if percentage < 0:
+        percentage = GUI.MIN_BRIGHTNESS
+
+    overheadWhiteLED.pwm(percentage)
 
 
 if __name__ == "__main__":
+    Device.pin_factory = MockFactory()
+
     gui = GUI()
     gui.start()
 
+    currentFanState = gui.isFanOn
+    currentFanDuration = gui.fanRunDurationInSeconds
+    currentFanPercentage = gui.fanSpeedInPercentage
+    currentOverheadBrightness = gui.whiteLedBrightnessInPercentage
+    currentButtonLedBrightness = gui.redLedBrightnessInPercentage
+
+    # Button element references
+    self.on_button = None
+    self.off_button = None
+
+    fans = [fan0, fan1]
+    leftButton = [leftButtonLED, leftButtonAction]
+    rightButton = [rightButtonLED, rightButtonAction]
 
     while True:
-        leftActionButton.when_pressed = lambda: turn_on_fan(duration)
-        rightActionButton.when_pressed = lambda: turn_on_fan(duration)
-        sleep(1)
+        if isDoorReady:
+            leftButtonAction.when_pressed = lambda: turn_on_fans(
+                fans, currentFanDuration
+            )
+            rightButtonAction.when_pressed = lambda: turn_on_fans(
+                fans, currentFanDuration
+            )
+            sleep(0.100)
+
+        update_button_brightness(currentButtonLedBrightness)
+        update_overhead_brightness(currentOverheadBrightness)
